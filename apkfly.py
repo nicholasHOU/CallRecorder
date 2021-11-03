9e5c1fad91d62a0f6ae4398311ba7b8d3fa63032
maxinliang1 <maxinliang1@gome.com.cn>
2021-11-03 19:57:33 +0800

    修改打印颜色

--- apkfly.py
+++ apkfly.py
@@ -1198,13 +1198,13 @@ def cmd_compile_merge(args):
         print(mergeType7M)
 
 def printGreen(message):
-    print "\033[1;36m%s\033[0m" % message
+    print "\033[0;36m%s\033[0m" % message
 
 def printRed(message):
-    print "\033[1;31m%s\033[0m" % message
+    print "\033[0;31m%s\033[0m" % message
 
 def printYellow(message):
-    print "\033[1;33m%s\033[0m" % message
+    print "\033[0;33m%s\033[0m" % message
 
 def cmd_remote(args):
     set = args.set
